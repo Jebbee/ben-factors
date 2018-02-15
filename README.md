8c98e4d93010e123241977cd9e5347acf5ee0b71
Jebbee <jeffreywjohnson+git@gmail.com>
2018-02-14 18:55:52 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+http://designmodo.github.io/Flat-UI/
+
+http://www.bootstrap-ui.com/
+
+https://www.w3schools.com/bootstrap/default.asp
+
+https://getbootstrap.com/docs/4.0/getting-started/introduction/
---
+++ hello.js
@@ -0,0 +1,6 @@
+// https://developers.google.com/web/tools/chrome-devtools/javascript/
+// view-source:https://googlechrome.github.io/devtools-samples/debug-js/get-started
+
+function sayHello(firstName, lastName) {
+  return "Hello " + firstName + " " + lastName
+}
