ec4dc650b5bd143ac954053e4a623386500ea469
Jebbee <jeffreywjohnson+git@gmail.com>
2018-02-18 19:50:20 -0800

    Layout for displaying results

--- factors.js
+++ factors.js
@@ -1,8 +1,17 @@
+function factors(n) {
+  var factorsArray = []
+
+  for (i = 0; i < 5; i++) {
+    factorsArray.push(i + 1)
+  }
+
+  return factorsArray
+}
 
 function calculateGreatestCommonFactor(number1, number2) {
   return {
     number1: number1,
     number2: number2,
-    greatestCommonFactor: 'TODO Calculate greatest common factor'
+    greatestCommonFactor: 24
   }
 }
