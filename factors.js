60ff68d973f8b898aeeddbf7bf8903c1bde8a294
Nebbee <BenAndCocoAndCousin@gmail.com>
2018-03-11 13:59:31 -0700

    Slumbers was here

--- factors.js
+++ factors.js
@@ -1,5 +1,19 @@
 function calculateGreatestCommonFactor(number1, number2) {
+  return 188
 }
 
 function factors(number) {
+  var factorsArray = []
+
+  for (var i = 1; i <= number; i++) {
+    if (isFactor(number, i)) {
+      factorsArray.push(i)
+    }
+  }
+
+  return factorsArray
+}
+
+function isFactor(number, possibleFactor) {
+  return (number % possibleFactor) == 0
 }
