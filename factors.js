60115c7d1a0b72a8916e2a3395d59ef4eb39843a
Jebbee <jeffreywjohnson+git@gmail.com>
2018-03-10 18:17:59 -0800

    Placeholder methods for Ben to implement

--- factors.js
+++ factors.js
@@ -1,17 +1,5 @@
-function factors(number) {
-  var factorsArray = []
-
-  for (i = 0; i < number; i++) {
-    factorsArray.push(i + 1)
-  }
-
-  return factorsArray
+function calculateGreatestCommonFactor(number1, number2) {
 }
 
-function calculateGreatestCommonFactor(number1, number2) {
-  return {
-    number1: number1,
-    number2: number2,
-    greatestCommonFactor: 24
-  }
+function factors(number) {
 }
